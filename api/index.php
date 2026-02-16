1f69671fd020feb5a997d5274f910cec037bb5f8
LJPathay <alta60785@gmail.com>
2026-02-16 23:19:28 +0800

    test: return string from route

--- api/index.php
+++ api/index.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 
+// Force error reporting at the very top
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 define('LARAVEL_START', microtime(true));
 
 // 1. Load the autoloader
@@ -9,16 +13,11 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // 2. Setup environment for Vercel
 if (isset($_SERVER['VERCEL_URL'])) {
-    // Force logs to stderr
     putenv('LOG_CHANNEL=stderr');
-
-    // Create and use writable static storage
     $storagePath = '/tmp/storage';
     foreach (['', '/framework', '/framework/views', '/framework/sessions', '/framework/cache', '/framework/cache/data', '/logs'] as $dir) {
         if (!is_dir($storagePath . $dir)) @mkdir($storagePath . $dir, 0755, true);
     }
-    
-    // We set this here so Laravel knows where to write compiled views
     putenv("VIEW_COMPILED_PATH=$storagePath/framework/views");
 }
 
@@ -30,4 +29,5 @@ if (isset($_SERVER['VERCEL_URL'])) {
 }
 
 // 4. Handle Request
-$app->handleRequest(Request::capture());
+$request = Request::capture();
+$app->handleRequest($request);
