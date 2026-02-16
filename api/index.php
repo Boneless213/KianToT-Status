af7f0eb31538014e5153c5ba0931e1652eb310bf
LJPathay <alta60785@gmail.com>
2026-02-16 22:53:37 +0800

    fix: force script name for vercel

--- api/index.php
+++ api/index.php
@@ -1,8 +1,11 @@
 <?php
 
-// Show ALL errors for debugging the white screen
+// Force error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-// Forward the request to the public/index.php
+// Vercel doesn't always set the document root correctly
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
+// Forward to the real index
 require __DIR__ . '/../public/index.php';
