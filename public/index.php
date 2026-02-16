6e9d63cf43aed3c40a1266923e775aa91a471177
Boneless213 <mytiktok213@gmail.com>
2026-02-16 21:01:49 +0800

    debug: trace public/index.php diagnostics

--- public/index.php
+++ public/index.php
@@ -5,16 +5,25 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// DEBUG: public/index.php started
+echo "DEBUG: public/index.php started<br>";
+
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+echo "DEBUG: public/index.php - loading autoloader...<br>";
+require __DIR__ . '/../vendor/autoload.php';
+echo "DEBUG: public/index.php - autoloader loaded<br>";
 
 // Bootstrap Laravel and handle the request...
+echo "DEBUG: public/index.php - requiring app.php...<br>";
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
+echo "DEBUG: public/index.php - app.php loaded, handling request...<br>";
 $app->handleRequest(Request::capture());
+
+echo "DEBUG: public/index.php - finished<br>";
